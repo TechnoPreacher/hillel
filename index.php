3e8e3fa47d06d6c414fd0c1a477efce8c18d753c
TechnoPreacher <nik.nik.sulia@gmail.com>
2021-10-16 01:11:11 +0300

    lesson7

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+require 'vendor/autoload.php';
+
+
+use nikolaysulima\hillel\core\Router;
+use nikolaysulima\hillel\core\subfolder\SecondClass;
+
+
+$obj = new Router();
+
+echo $obj->run().' - это работа метода класса из core/</br>';
+
+echo 'а это свойство второго класса из core/subfolder:  '. (($obj2 = new SecondClass())->secondClassProperty1).'</br>';
+
+
